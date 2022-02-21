22e8b6e0328cbae012fbd5b58beb5c2053bfc91e
jyj <yunji0220@naver.com>
2022-02-21 13:53:49 +0900

    주석 삭제

--- src/common/PrivateRoute.js
+++ src/common/PrivateRoute.js
@@ -7,7 +7,7 @@ import {
   
 const PrivateRoute = ({ component: Component, authenticated, ...rest }) => (
     <Route
-      {...rest}     //rest 객체 안에 있는 모든 것들을 설정해주겠다는 의미. rest 객체에 뭐가 있지?
+      {...rest}    
       render={props =>
         authenticated ? (
           <Component {...rest} {...props} />
