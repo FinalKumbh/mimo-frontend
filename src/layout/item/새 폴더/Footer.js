2384ca0a5ce38aaad12ff01d47f658aab7d6401d
mina <myelin24@naver.com>
2022-03-01 18:58:17 +0900

    0301

--- src/layout/item/새 폴더/Footer.js
+++ src/layout/item/새 폴더/Footer.js
@@ -5,18 +5,18 @@ import { REVIEW , HOME , MYPAGE, CAMERA} from '../../constants';
  
 
 
-export  function FooterNav() {
+export  function  Footer() {
   return (
     <div>
                      
         <hr />
             <div className="icon">
-               <NavLink to="/main">  <img id="eimage2" src={process.env.PUBLIC_URL+`assets/image/home.png`}  /></NavLink>
+              <NavLink to="/main">  <img id="eimage2" src={process.env.PUBLIC_URL+`assets/image/home.png`}  /></NavLink>
+
                <NavLink to="/review"> <img id="eimage2" src={process.env.PUBLIC_URL+`assets/image/review.png`}  /></NavLink>
-               <NavLink to="/modelhome"> <img id="eimage2" src={process.env.PUBLIC_URL+`assets/image/camera.png`}  /></NavLink>
+               <NavLink to="/camera"> <img id="eimage2" src={process.env.PUBLIC_URL+`assets/image/camera.png`}  /></NavLink>
                <NavLink to="/profile"> <img id="eimage2" src={process.env.PUBLIC_URL+`assets/image/mypage.png`}  /></NavLink>
 
-               <NavLink to="/review2"> <img id="eimage2" src={process.env.PUBLIC_URL+`assets/image/review.png`}  /></NavLink>
 
             </div>
 
@@ -31,3 +31,6 @@ export  function FooterNav() {
 
 
 
+
+
+export default Footer
